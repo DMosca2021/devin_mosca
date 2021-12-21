9eebfbedf69cdb7aeac3bf695ca7da89fce27d6a
Devin Mosca <devinmosca@yahoo.com>
2021-12-21 01:29:47 -0700

    Added basic styling for contact form

--- client/src/components/Contact.js
+++ client/src/components/Contact.js
@@ -1,8 +1,96 @@
 function Contact() {
     return (
-        <div>
-            <h1>This is where a contact form will go to get in touch with me</h1>
-            <p>Contact form must have functionality</p>
+        <div className='container is-fluid columns is-mobile'>
+            <div className='column is-10 is-offset-1'>
+                <section className='columns is-multiline section is-large'>
+                    <form className='box column is-8'>
+                        <div class="field">
+                            <label className="label">Name</label>
+                            <div className="control">
+                                <input className="input" type="text" placeholder="Text input"></input>
+                            </div>
+                        </div>
+
+                        <div className="field">
+                            <label className="label">Username</label>
+                            <div className="control has-icons-left has-icons-right">
+                                <input className="input is-success" type="text" placeholder="Text input" value="bulma"></input>
+                                <span className="icon is-small is-left">
+                                    <i className="fas fa-user"></i>
+                                </span>
+                                <span className="icon is-small is-right">
+                                    <i className="fas fa-check"></i>
+                                </span>
+                            </div>
+                            <p className="help is-success">This username is available</p>
+                        </div>
+
+                        <div className="field">
+                            <label className="label">Email</label>
+                            <div className="control has-icons-left has-icons-right">
+                                <input className="input is-danger" type="email" placeholder="Email input" value="hello@"></input>
+                                <span className="icon is-small is-left">
+                                    <i className="fas fa-envelope"></i>
+                                </span>
+                                <span className="icon is-small is-right">
+                                    <i className="fas fa-exclamation-triangle"></i>
+                                </span>
+                            </div>
+                            <p className="help is-danger">This email is invalid</p>
+                        </div>
+
+                        <div className="field">
+                            <label className="label">Subject</label>
+                            <div className="control">
+                                <div className="select">
+                                    <select>
+                                        <option>Select dropdown</option>
+                                        <option>With options</option>
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="field">
+                            <label className="label">Message</label>
+                            <div className="control">
+                                <textarea className="textarea" placeholder="Textarea"></textarea>
+                            </div>
+                        </div>
+
+                        <div className="field">
+                            <div className="control">
+                                <label className="checkbox">
+                                    <input type="checkbox"></input>
+                                    I agree to the <a href="#">terms and conditions</a>
+                                </label>
+                            </div>
+                        </div>
+
+                        <div className="field">
+                            <div className="control">
+                                <label className="radio">
+                                    <input type="radio" name="question"></input>
+                                    Yes
+                                </label>
+                                <label className="radio">
+                                    <input type="radio" name="question"></input>
+                                    No
+                                </label>
+                            </div>
+                        </div>
+
+                        <div className="field is-grouped">
+                            <div className="control">
+                                <button className="button is-link">Submit</button>
+                            </div>
+                            <div className="control">
+                                <button className="button is-link is-light">Cancel</button>
+                            </div>
+                        </div>
+                    </form>
+                </section>
+            </div>
         </div>
     )
 }
