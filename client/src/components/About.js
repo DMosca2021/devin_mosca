7b9428d092442c7583988887e864ab337a3e40e2
dMosca <devinmosca@yahoo.com>
2021-12-20 17:33:08 -0700

    Removed prepackaged react info

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -4,7 +4,7 @@ function About() {
             <h1>This is where the about me info will go</h1>
             <h2>Maybe add my brand statement</h2>
             <h3>Maybe have a picture of myself here</h3>
-            <img alt="picture"></img>
+            <img alt="profile"></img>
         </div>
     )
 }
