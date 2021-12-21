bb02b60a5241697c6d60e218f617ceb27cc4c6c0
Devin Mosca <devinmosca@yahoo.com>
2021-12-21 02:21:46 -0700

    Added basic styling to skills section

--- client/src/components/Skills.js
+++ client/src/components/Skills.js
@@ -1,12 +1,15 @@
+import { Carousel } from '3d-react-carousal'
+
 function Skills() {
+    let slides = ["HTML", "CSS", "JavaScript", "Git", "Node.js", "REST API", "Handlebars", "Express.js", "MySQL", "Sequelize", "MongoDB", "Mongoose", "React.js", "Redux", "GraphQL", "Apollo", "Phaser", "C++", "Unreal 4", "Illustrator", "Photoshop", <div className="box">Hello</div>];
+
     return (
-        <div>
-            <h1>This will be where my skills go, maybe in a list:</h1>
-            <ul>
-                <li>HTML</li>
-                <li>CSS</li>
-                <li>JavaScript</li>
-            </ul>
+        <div className='container is-fluid columns is-mobile'>
+            <div className='column is-10 is-offset-1'>
+                <section className='box'>
+                    <Carousel slides={slides}></Carousel>
+                </section>
+            </div>
         </div>
     )
 }
