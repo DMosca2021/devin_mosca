4dc6929cc232a7df5875cb1e8d2739cf57a6cc52
Devin Mosca <devinmosca@yahoo.com>
2021-12-21 22:26:08 -0700

    Added css and handlebars logos

--- client/src/components/Skills.js
+++ client/src/components/Skills.js
@@ -1,22 +1,23 @@
 import { Carousel } from '3d-react-carousal';
 import htmlLogo from '../assets/svg/html_logo.svg';
-// import cssLogo from '../assets/svg/css_logo.svg';
+import cssLogo from '../assets/svg/css_logo.svg';
 import jsLogo from '../assets/svg/javascript_logo.svg';
 import gitLogo from '../assets/svg/git_logo.svg';
 import nodeLogo from '../assets/svg/nodejs_logo.svg';
 import restLogo from '../assets/images/restAPI_logo.jpg';
+import handleLogo from '../assets/svg/handlebars_logo.svg';
 
 
 
 function Skills() {
     let slides = [
         <img src={htmlLogo} alt="HTML" id='tech-logo'></img>,
-        "CSS",
+        <img src={cssLogo} alt="CSS" id='tech-logo'></img>,
         <img src={jsLogo} alt='JavaScript' id='tech-logo'></img>,
         <img src={gitLogo} alt="Git" id='tech-logo'></img>,
         <img src={nodeLogo} alt="Node.js" id='tech-logo'></img>,
         <img src={restLogo} alt="REST API" id='tech-logo'></img>,
-        "Handlebars",
+        <img src={handleLogo} alt="Handlebars" id='tech-logo'></img>,
         "Express.js",
         "MySQL",
         "Sequelize",
@@ -34,11 +35,12 @@ function Skills() {
         <div className="box">Hello</div>];
 
     return (
-        <div className='container is-fluid columns is-mobile' id='skills-contain'>
-            <div className='column'>
+        <div className='container is-fluid columns is-centered is-vcentered is-mobile' id='skills-contain'>
+            <div className='column has-text-centered'>
+                <h1 className="title is-1" id='skills-title'>Some technology I am familiar with</h1>
                 <section className='section is-large '>
-                    <h1 className="title is-1 ">Title</h1>
-                    <h2 className="subtitle">Subtitle</h2>
+                <h2 className="subtitle is-3">Use the arrows or swipe through the gallery</h2>
+
                     <Carousel slides={slides}></Carousel>
                 </section>
             </div>
