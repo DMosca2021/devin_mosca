e27697f6de03b154b78caee54ea02139e388d2b5
Devin Mosca <devinmosca@yahoo.com>
2021-12-27 16:14:11 -0700

    Removed flip animation, css conflicts. Added Eternal External images

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # devin_mosca-portfolio
 My professional portfolio to showcase my coding skillset and completed projects
+
+https://www.npmjs.com/package/react-image-gallery
--- client/src/components/Projects.js
+++ client/src/components/Projects.js
@@ -2,6 +2,14 @@ import ImageGallery from 'react-image-gallery';
 import sobHome from '../assets/images/sob_home_scrnsht.png';
 import sobSignUp from '../assets/images/sob_log-in_scrnsht.png';
 import sobLogIn from '../assets/images/sob_sign-up_scrnsht.png';
+import eeHome from '../assets/images/EtEx_homepage.png';
+import eeAdvice from '../assets/images/EtEx_advicedropdown.png';
+import eeGraph from '../assets/images/EtEx_graph.png';
+import eeModal from '../assets/images/EtEx_modal.png';
+import eeQuiz from '../assets/images/EtEx_quiz.png';
+import eeEmotion from '../assets/images/EtEx_textentryemotion.png';
+import eeUserOne from '../assets/images/EtEx_user1name.png';
+import eeUserTwo from '../assets/images/EtEx_user2name.png';
 
 function Projects() {
   const sobImages = [
@@ -16,7 +24,32 @@ function Projects() {
     },
   ];
 
-  const eeImages = [];
+  const eeImages = [
+    {
+      original: eeHome,
+    },
+    {
+      original: eeUserTwo,
+    },
+    {
+      original: eeQuiz,
+    },
+    {
+      original: eeUserOne,
+    },
+    {
+      original: eeAdvice,
+    },
+    {
+      original: eeModal,
+    },
+    {
+      original: eeEmotion,
+    },
+    {
+      original: eeGraph,
+    },
+  ];
 
   const weatherImages = [];
 
@@ -24,26 +57,26 @@ function Projects() {
     <div className='container is-fluid columns is-mobile' id='project-contain'>
       <div className='column'>
         <section className='section is-large' id='temp-border'>
-          <div className='tile is-ancestor'>
+          <div className='tile is-ancestor' id='temp-border-red'>
             <div className='tile is-parent' id='flip-card'>
               <article className='tile is-child box' id='flip-card-inner'>
-                <figure className='image is-4by3' id='flip-card-front'>
+                {/* <figure className='image is-4by3' id='flip-card-front'>
                   <img
                     src='https://bulma.io/images/placeholders/640x480.png'
                     alt='screenshot'
                   ></img>
                   <p className='title'>Scrypts of Betrayal</p>
                   <p className='subtitle'>With an image</p>
-                </figure>
+                </figure> */}
                 <div id='flip-card-back'>
                   <p className='title'>Scrypts of Betrayal</p>
                   <p className='subtitle'>With an image</p>
                   <figure className='image is-4by3'>
-                    <ImageGallery 
-                    items={sobImages}
-                    showPlayButton={false}
-                    showNav={false}
-                    showBullets={true}
+                    <ImageGallery
+                      items={sobImages}
+                      showPlayButton={false}
+                      showNav={false}
+                      showBullets={true}
                     />
                   </figure>
                   <div className='content'>
@@ -63,48 +96,78 @@ function Projects() {
               </article>
             </div>
 
-            <div className='tile is-parent' id='temp-border'>
-              <article className='tile is-child box'>
-                <p className='title'>Eternal External</p>
-                <p className='subtitle'>With an image</p>
-                <figure className='image is-4by3'>
-                  <img src='https://bulma.io/images/placeholders/640x480.png'></img>
-                </figure>
-                <div className='content'>
-                  <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    Proin ornare magna eros, eu pellentesque tortor vestibulum
-                    ut. Maecenas non massa sem. Etiam finibus odio quis feugiat
-                    facilisis.
-                  </p>
-                </div>
-                <div class='buttons has-addons is-centered'>
-                  <button class='button'>Deployed App</button>
-                  <button class='button'>Repository </button>
-                  {/* <button class='button'>No</button> */}
+            <div className='tile is-parent' id='flip-card'>
+              <article className='tile is-child box' id='flip-card-inner'>
+                {/* <figure className='image is-4by3' id='flip-card-front'>
+                  <img
+                    src='https://bulma.io/images/placeholders/640x480.png'
+                    alt='screenshot'
+                  ></img>
+                  <p className='title'>Scrypts of Betrayal</p>
+                  <p className='subtitle'>With an image</p>
+                </figure> */}
+                <div id='flip-card-back'>
+                  <p className='title'>Eternal External</p>
+                  <p className='subtitle'>With an image</p>
+                  <figure className='image is-4by3'>
+                    <ImageGallery
+                      items={eeImages}
+                      showPlayButton={false}
+                      showNav={false}
+                      showBullets={true}
+                    />
+                  </figure>
+                  <div className='content'>
+                    <p>
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                      Proin ornare magna eros, eu pellentesque tortor vestibulum
+                      ut. Maecenas non massa sem. Etiam finibus odio quis
+                      feugiat facilisis.
+                    </p>
+                  </div>
+                  <div class='buttons has-addons is-centered'>
+                    <button class='button'>Deployed App</button>
+                    <button class='button'>Repository </button>
+                    {/* <button class='button'>No</button> */}
+                  </div>
                 </div>
               </article>
             </div>
 
-            <div className='tile is-parent' id='temp-border'>
-              <article className='tile is-child box'>
-                <p className='title'>Devin's Weather Dashboard</p>
-                <p className='subtitle'>With an image</p>
-                <figure className='image is-4by3'>
-                  <img src='https://bulma.io/images/placeholders/640x480.png'></img>
-                </figure>
-                <div className='content'>
-                  <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    Proin ornare magna eros, eu pellentesque tortor vestibulum
-                    ut. Maecenas non massa sem. Etiam finibus odio quis feugiat
-                    facilisis.
-                  </p>
-                </div>
-                <div class='buttons has-addons is-centered'>
-                  <button class='button'>Deployed App</button>
-                  <button class='button'>Repository </button>
-                  {/* <button class='button'>No</button> */}
+            <div className='tile is-parent' id='flip-card'>
+              <article className='tile is-child box' id='flip-card-inner'>
+                {/* <figure className='image is-4by3' id='flip-card-front'>
+                  <img
+                    src='https://bulma.io/images/placeholders/640x480.png'
+                    alt='screenshot'
+                  ></img>
+                  <p className='title'>Scrypts of Betrayal</p>
+                  <p className='subtitle'>With an image</p>
+                </figure> */}
+                <div id='flip-card-back'>
+                  <p className='title'>Devin's Weather Dashboard</p>
+                  <p className='subtitle'>With an image</p>
+                  <figure className='image is-4by3'>
+                    <ImageGallery
+                      items={sobImages}
+                      showPlayButton={false}
+                      showNav={false}
+                      showBullets={true}
+                    />
+                  </figure>
+                  <div className='content'>
+                    <p>
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                      Proin ornare magna eros, eu pellentesque tortor vestibulum
+                      ut. Maecenas non massa sem. Etiam finibus odio quis
+                      feugiat facilisis.
+                    </p>
+                  </div>
+                  <div class='buttons has-addons is-centered'>
+                    <button class='button'>Deployed App</button>
+                    <button class='button'>Repository </button>
+                    {/* <button class='button'>No</button> */}
+                  </div>
                 </div>
               </article>
             </div>
