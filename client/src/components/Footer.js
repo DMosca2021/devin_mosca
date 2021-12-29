bf4af5b746e8d9afa2cef809d62294e2122d5e14
Devin Mosca <devinmosca@yahoo.com>
2021-12-28 21:35:54 -0700

    Footer nav links working

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -4,19 +4,19 @@ function Footer() {
       <footer className='footer column is-full' id='footer-contain'>
         <nav className='level'>
           <p className='level-item has-text-centered'>
-            <a className='link is-info'>Back to the top</a>
+            <a className='link is-info' href='#nav-contain'>Back to the top</a>
           </p>
           <p className='level-item has-text-centered'>
-            <a className='link is-info'>My Skills</a>
+            <a className='link is-info' href='#skills-contain'>My Skills</a>
           </p>
           <p className='level-item has-text-centered'>
             <img src='https://bulma.io/images/bulma-type.png' alt='logo'></img>
           </p>
           <p className='level-item has-text-centered'>
-            <a className='link is-info'>My Projects</a>
+            <a className='link is-info' href='#project-contain'>My Projects</a>
           </p>
           <p className='level-item has-text-centered'>
-            <a className='link is-info'>Contact Me</a>
+            <a className='link is-info' href='#form-section'>Contact Me</a>
           </p>
         </nav>
       </footer>
